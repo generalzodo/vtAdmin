debd94447f185f69fa3053727eb20a35a8a466da
generalzodo <somto8000@gmail.com>
2023-12-30 19:57:16 +0100

    done

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit {
   menus: any = [
-    {title: 'Dashboard', url:''},
-    {title: 'Users', url:'users'},
-    {title: 'Bookings', url:'bookings'},
-    {title: 'Trips', url:'trips'},
-    {title: 'Buses', url:'buses'},
-    {title: 'Drivers', url:'drivers'},
-    {title: 'Routes', url:'routes'},
-    {title: 'Settings', url:'settings'},
+    { title: 'Dashboard', url: '' },
+    { title: 'Users', url: 'users' },
+    { title: 'Bookings', url: 'bookings' },
+    { title: 'Trips', url: 'trips' },
+    { title: 'Buses', url: 'buses' },
+    { title: 'Drivers', url: 'drivers' },
+    { title: 'Routes', url: 'routes' },
+    { title: 'Settings', url: 'settings' },
   ]
+  currentRoute: string;
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    // this.view()
+  }
+
+  view() {
+    setTimeout(() => {
+      
+      this.currentRoute = this.router.url.substring(1);
+      console.log(this.currentRoute)
+    }, 500);
+  }
 }
+
